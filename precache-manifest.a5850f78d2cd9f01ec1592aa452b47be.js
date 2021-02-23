e347056144047c65f1ac7334072bac7a42079167
Jhony Pereira <jhonystein@gmail.com>
2021-02-23 15:25:38 -0300

    deploy

---
+++ precache-manifest.a5850f78d2cd9f01ec1592aa452b47be.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a2370f5794fd7926914",
+    "url": "/marketing-land/css/app.a77fac52.css"
+  },
+  {
+    "revision": "5821e59bdce71291916e",
+    "url": "/marketing-land/css/chunk-vendors.b036e5c1.css"
+  },
+  {
+    "revision": "35d78619c919eff7a51438f160fd1a11",
+    "url": "/marketing-land/icone.png"
+  },
+  {
+    "revision": "496f4c0281800d55226e129f0c5f1b97",
+    "url": "/marketing-land/img/banner2.496f4c02.png"
+  },
+  {
+    "revision": "35d78619c919eff7a51438f160fd1a11",
+    "url": "/marketing-land/img/im01.35d78619.png"
+  },
+  {
+    "revision": "7ba1d97e9548db54d312efc00a583777",
+    "url": "/marketing-land/img/im02.7ba1d97e.png"
+  },
+  {
+    "revision": "d0309407d488aef037734562375710cd",
+    "url": "/marketing-land/img/im03.d0309407.png"
+  },
+  {
+    "revision": "89af21b5fc2cfaaa503f94fa92c40c0b",
+    "url": "/marketing-land/img/im04.89af21b5.png"
+  },
+  {
+    "revision": "0cbc52472aa243abcfed9c83fdccdc63",
+    "url": "/marketing-land/img/logo.0cbc5247.png"
+  },
+  {
+    "revision": "43219c0167216cc2c8dacbe9efcc1c31",
+    "url": "/marketing-land/img/whatsapp.43219c01.png"
+  },
+  {
+    "revision": "95d870d67b6431bfb73a1d395649544a",
+    "url": "/marketing-land/index.html"
+  },
+  {
+    "revision": "0a2370f5794fd7926914",
+    "url": "/marketing-land/js/app.a8fdff3a.js"
+  },
+  {
+    "revision": "5821e59bdce71291916e",
+    "url": "/marketing-land/js/chunk-vendors.2909608e.js"
+  },
+  {
+    "revision": "d1192ee39b9dc18785ec4af6eadbb8cc",
+    "url": "/marketing-land/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/marketing-land/robots.txt"
+  }
+]);
